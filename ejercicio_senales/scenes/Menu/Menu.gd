f847b74af992d4eaeeadba181b1338dbd5ac51e1
luciamalena <luciamalena20@gmail.com>
2026-04-22 20:38:29 -0300

    Conexión de la escena menú a las instancias

--- ejercicio_senales/scenes/Menu/Menu.gd
+++ ejercicio_senales/scenes/Menu/Menu.gd
@@ -3,14 +3,19 @@ extends Node2D
 const INSTANCES_TEXT = "Cantidad de instancias: "
 # Variable para contar cantidad de instancias
 var instancesCount = 0
+@onready var redinstancia = $RedGenerator
+@onready var violetinstancia = $VioletGenerator
 
 func _ready():
 	# Inicializa el texto de "CountLabel" usando el texto
 	# Constante "INSTANCES_TEXT" más el valor de "instancesCount" como Texto
 	$CountLabel.text = INSTANCES_TEXT + str(instancesCount)
-	pass
+	if redinstancia:
+		redinstancia.button_down.connect(count_new_instance)
+	if violetinstancia:
+		violetinstancia.button_down.connect(count_new_instance)
 
 func count_new_instance():
-	instancesCount = +1
+	instancesCount += 1
 	$CountLabel.text = INSTANCES_TEXT + str(instancesCount)
 	
